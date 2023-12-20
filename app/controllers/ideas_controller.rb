e16f17036858cc4537812490567a84a84fc68ebf
parash-namespace <parash.a@namespace.jp>
2023-12-20 14:09:13 +0545

    Add flash messages

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,9 +7,9 @@ class IdeasController < ApplicationController
     @idea = Idea.create(idea_params)
 
     if @idea.valid?
-      # Implement later
+      flash[:success] = "Your idea has been posted!"
     else
-      # Implement later
+      flash[:alert] = "Woops! Looks like there has been an error!"
     end
   
     redirect_to root_path
@@ -22,8 +22,12 @@ class IdeasController < ApplicationController
   def update
     @idea = Idea.find(params[:id])
     if @idea.update(idea_params)
+      flash[:success] = "The idea has been updated!"
+
       redirect_to root_path
     else
+      flash[:alert] = "Woops! Looks like there has been an error!"
+
       redirect_to edit_idea_path(params[:id])
     end
   end
@@ -31,6 +35,8 @@ class IdeasController < ApplicationController
   def destroy
     @idea = Idea.find(params[:id])
     @idea.destroy
+    flash[:success] = "The idea was successfully deleted!"
+
     redirect_to root_path
   end  
 
